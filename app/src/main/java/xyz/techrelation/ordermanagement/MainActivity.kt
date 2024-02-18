6122de9b2efafe9d29a22c1abdbc6c1e25e1262d
AkihiroTokai <contact@techrelation.xyz>
2024-02-18 15:36:19 +0900

    add item_View

--- app/src/main/java/xyz/techrelation/ordermanagement/MainActivity.kt
+++ app/src/main/java/xyz/techrelation/ordermanagement/MainActivity.kt
@@ -17,7 +17,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var zenzai: TextView
     private lateinit var payment: TextView
     private lateinit var remarks: TextView
+
     private lateinit var recyclerView: RecyclerView
+
+    private lateinit var item_id: TextView
+    private lateinit var item_time: TextView
+    private lateinit var item_taiyaki_normal: TextView
+    private lateinit var item_taiyaki_special: TextView
+    private lateinit var item_dango: TextView
+    private lateinit var item_zenzai: TextView
+    private lateinit var item_payment: TextView
+    private lateinit var item_remarks: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,12 +38,31 @@ class MainActivity : AppCompatActivity() {
         notServed = this.findViewById(R.id.notServed)
         notPaid = this.findViewById(R.id.notPaid)
         done = this.findViewById(R.id.done)
+
         id = this.findViewById(R.id.id)
+        time = this.findViewById(R.id.time)
+        taiyaki_normal = this.findViewById(R.id.taiyaki_normal)
+        taiyaki_special = this.findViewById(R.id.taiyaki_special)
+        dango = this.findViewById(R.id.dango)
+        zenzai = this.findViewById(R.id.zenzai)
+        payment = this.findViewById(R.id.payment)
+        remarks = this.findViewById(R.id.remarks)
+
         recyclerView = this.findViewById(R.id.recyclerView)
+
+        item_id = this.findViewById(R.id.item_id)
+        item_time = this.findViewById(R.id.item_time)
+        item_taiyaki_normal = this.findViewById(R.id.item_taiyaki_normal)
+        item_taiyaki_special = this.findViewById(R.id.item_taiyaki_special)
+        item_dango = this.findViewById(R.id.item_dango)
+        item_zenzai = this.findViewById(R.id.item_zenzai)
+        item_payment = this.findViewById(R.id.item_payment)
+        item_remarks = this.findViewById(R.id.item_remarks)
 
 
         notServed.setOnClickListener{
             selectedTable = 0
+
             id.setBackgroundResource(R.drawable.frame_style_notserved)
             time.setBackgroundResource(R.drawable.frame_style_notserved)
             taiyaki_normal.setBackgroundResource(R.drawable.frame_style_notserved)
@@ -42,10 +71,21 @@ class MainActivity : AppCompatActivity() {
             zenzai.setBackgroundResource(R.drawable.frame_style_notserved)
             payment.setBackgroundResource(R.drawable.frame_style_notserved)
             remarks.setBackgroundResource(R.drawable.frame_style_notserved)
+
+            item_id.setBackgroundResource(R.drawable.frame_style_notserved)
+            item_time.setBackgroundResource(R.drawable.frame_style_notserved)
+            item_taiyaki_normal.setBackgroundResource(R.drawable.frame_style_notserved)
+            item_taiyaki_special.setBackgroundResource(R.drawable.frame_style_notserved)
+            item_dango.setBackgroundResource(R.drawable.frame_style_notserved)
+            item_zenzai.setBackgroundResource(R.drawable.frame_style_notserved)
+            item_payment.setBackgroundResource(R.drawable.frame_style_notserved)
+            item_remarks.setBackgroundResource(R.drawable.frame_style_notserved)
+
         }
 
         notPaid.setOnClickListener{
             selectedTable = 1
+
             id.setBackgroundResource(R.drawable.frame_style_notpaid)
             time.setBackgroundResource(R.drawable.frame_style_notpaid)
             taiyaki_normal.setBackgroundResource(R.drawable.frame_style_notpaid)
@@ -54,10 +94,20 @@ class MainActivity : AppCompatActivity() {
             zenzai.setBackgroundResource(R.drawable.frame_style_notpaid)
             payment.setBackgroundResource(R.drawable.frame_style_notpaid)
             remarks.setBackgroundResource(R.drawable.frame_style_notpaid)
+
+            item_id.setBackgroundResource(R.drawable.frame_style_notpaid)
+            item_time.setBackgroundResource(R.drawable.frame_style_notpaid)
+            item_taiyaki_normal.setBackgroundResource(R.drawable.frame_style_notpaid)
+            item_taiyaki_special.setBackgroundResource(R.drawable.frame_style_notpaid)
+            item_dango.setBackgroundResource(R.drawable.frame_style_notpaid)
+            item_zenzai.setBackgroundResource(R.drawable.frame_style_notpaid)
+            item_payment.setBackgroundResource(R.drawable.frame_style_notpaid)
+            item_remarks.setBackgroundResource(R.drawable.frame_style_notpaid)
         }
 
         done.setOnClickListener{
             selectedTable = 2
+
             id.setBackgroundResource(R.drawable.frame_style_done)
             time.setBackgroundResource(R.drawable.frame_style_done)
             taiyaki_normal.setBackgroundResource(R.drawable.frame_style_done)
@@ -66,6 +116,15 @@ class MainActivity : AppCompatActivity() {
             zenzai.setBackgroundResource(R.drawable.frame_style_done)
             payment.setBackgroundResource(R.drawable.frame_style_done)
             remarks.setBackgroundResource(R.drawable.frame_style_done)
+
+            item_id.setBackgroundResource(R.drawable.frame_style_done)
+            item_time.setBackgroundResource(R.drawable.frame_style_done)
+            item_taiyaki_normal.setBackgroundResource(R.drawable.frame_style_done)
+            item_taiyaki_special.setBackgroundResource(R.drawable.frame_style_done)
+            item_dango.setBackgroundResource(R.drawable.frame_style_done)
+            item_zenzai.setBackgroundResource(R.drawable.frame_style_done)
+            item_payment.setBackgroundResource(R.drawable.frame_style_done)
+            item_remarks.setBackgroundResource(R.drawable.frame_style_done)
         }
 
 
